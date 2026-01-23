13992ee116da80d452179655c98fc7c45f71b0ec
nt_fox <nt_fox@foxmail.com>
2026-01-23 16:28:55 +0800

    update

--- Script2/Script2.cs
+++ Script2/Script2.cs
@@ -8,7 +8,6 @@ namespace Script2
     {
         // 静态字段：统一的参数
         private static readonly ParameterExpression _envParam = Expression.Parameter(typeof(Script2Environment), "env");
-        private static readonly Dictionary<string, Expression> _funcDecls = new();
         // 标记当前解析的函数是否有return语句
         // 使用 ThreadLocal 是因为：1) 解析器是静态的，需要在线程间隔离状态；2) ReturnStatement 在解析时需要修改外层 FuncDecl 的状态
         private static readonly ThreadLocal<bool> _hasReturnInFunction = new(() => false);
